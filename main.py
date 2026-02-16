048c36ffca34874229fd341181889f1eae3b9fc8
pugin.ilya <pugin.ilya@rwb.ru>
2026-02-16 17:18:44 +0300

    ci: Добавлена deploy конфигурация

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from src.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app=app, host="0.0.0.0", port=8080, workers=1)
+    uvicorn.run(app=app, host="0.0.0.0", port=8000, workers=2)
