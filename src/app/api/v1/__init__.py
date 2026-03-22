e7b69bc407b2a900a72fc13d4fbbb600a0308281
pugin.ilya <pugin.ilya@rwb.ru>
2026-03-23 01:11:43 +0300

    refactor: Переименован путь для генерации RWB Data (/api/v1/dto/avro-ocf)

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from .table import router as table_router
 from .openmetadata import router as openmetadata_router
-from .avro import router as avro_router
+from .dto import router as dto_router
 
 router = APIRouter(prefix="/v1")
 router.include_router(router=table_router)
 router.include_router(router=openmetadata_router)
-router.include_router(router=avro_router)
+router.include_router(router=dto_router)
