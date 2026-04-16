e043dc1fe59958b4bd38bb22d57b21952cf8e432
pugin.ilya <pugin.ilya@rwb.ru>
2026-04-16 16:45:48 +0300

    fix: Переименован аргумент Field (example -> examples)

--- src/app/models/request.py
+++ src/app/models/request.py
@@ -8,13 +8,13 @@ from src.sources.avro_ocf import AvroBase64Str, AvroFilenameStr
 class SyntheticRequest(BaseModel):
     source: SourceConfig = Field(..., description="Data source configuration.")
     output_size: int = Field(
-        ..., gt=0, example=5, description="How many rows to generate"
+        ..., gt=0, examples=[100], description="How many rows to generate"
     )
     load_limit: Optional[int] = Field(
         None,
         gt=0,
         description="Maximum number of rows to load from the source for training.",
-        example=1000,
+        examples=[1000],
     )
 
 
