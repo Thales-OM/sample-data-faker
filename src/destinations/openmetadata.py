687326e9d04e0c7fb48abc5f94f5accb41149fdd
pugin.ilya <pugin.ilya@rwb.ru>
2026-05-01 15:13:46 +0300

    fix(module:destinations): Instead of separate model config add clasmethod for developer config submit

--- src/destinations/openmetadata.py
+++ src/destinations/openmetadata.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import pandas as pd
 import pyarrow as pa
-from pydantic import Field, BaseModel
+from pydantic import Field
 from typing import Any, Dict, Union, Literal
 from src.openmetadata import SampleData
 from src.logger import LoggerFactory
@@ -22,17 +22,21 @@ class OpenMetadataDestinationConfig(BaseDestinationConfig, OMDConfig):
     table_id: str = Field(description="ID of the table to attach SampleData to.")
 
 
-class OpenMetadataDestinationConfigInternal(BaseModel):
-    table_id: str = Field(description="ID of the table to attach SampleData to.")
-    omd_async_client: AsyncOMDClient = Field(
-        description="Provides an existing client instead of creating a new one. \
-            For internal use. Client is not closed upon execution.",
-    )
+@dataclass
+class OpenMetadataDestinationConfigInternal:
+    """
+    For internal use. Not exposed in schema.
+    Provides an existing client instead of creating a new one.
+    Client is not closed upon execution.
+    """
+
+    table_id: str
+    omd_async_client: AsyncOMDClient
 
 
 class OpenMetadataDestination(BaseDestination):
     type: Literal["openmetadata"]
-    config: Union[OpenMetadataDestinationConfig, OpenMetadataDestinationConfigInternal]
+    config: OpenMetadataDestinationConfig
 
     async def submit(
         self, data: Union[pd.DataFrame, pa.Table]
@@ -40,20 +44,33 @@ class OpenMetadataDestination(BaseDestination):
         if isinstance(data, pa.Table):
             data = data.to_pandas()
 
-        if isinstance(self.config, OpenMetadataDestinationConfigInternal):
-            return await self._submit_with_client(
-                df=data,
-                table_id=self.config.table_id,
-                client=self.config.omd_async_client,
-            )
-
         async with AsyncOMDClient(config=self.config, logger=logger) as client:
             return await self._submit_with_client(
                 df=data, table_id=self.config.table_id, client=client
             )
 
+    @classmethod
+    async def submit_with_own_client(
+        cls,
+        data: Union[pd.DataFrame, pa.Table],
+        config: OpenMetadataDestinationConfigInternal,
+    ) -> OpenMetadataDestinationResponse:
+        """
+        Bypass model validation for standard config and
+        submit via preconfigured client. Client is not closed.
+        """
+        if isinstance(data, pa.Table):
+            data = data.to_pandas()
+
+        return await cls._submit_with_client(
+            df=data,
+            table_id=config.table_id,
+            client=config.omd_async_client,
+        )
+
+    @staticmethod
     async def _submit_with_client(
-        self, df: pd.DataFrame, table_id: str, client: AsyncOMDClient
+        df: pd.DataFrame, table_id: str, client: AsyncOMDClient
     ) -> OpenMetadataDestinationResponse:
         sample_data = SampleData.from_dataframe(df=df)
         table = await client.add_sample_data(id=table_id, body=sample_data)
